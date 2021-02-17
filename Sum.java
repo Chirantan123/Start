0423488e27089aef2632d320b27a9c8df7895059
Chirantan123 <chirantanism@gmail.com>
2021-02-17 10:00:06 +0530

    Substraction changed with extra variables

--- Sum.java
+++ Sum.java
@@ -2,7 +2,9 @@ class Sum{
     public static void main(String args[])
     {
         int a=1,b=2;
-        int c=a-b;
-        System.out.println("Substraction result= " + c);
+        int c=a;
+        int d=b;
+        int z=c-d;
+        System.out.println("Substraction result= " + z);
     }
 }
